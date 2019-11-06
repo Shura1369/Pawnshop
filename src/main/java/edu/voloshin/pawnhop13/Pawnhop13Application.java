6a09c29768d3e622e55ad04cc404fb28e88008ef
Shura1369 <51516532+Shura1369@users.noreply.github.com>
2019-11-06 12:42:45 +0200

    new

---
+++ src/main/java/edu/voloshin/pawnhop13/Pawnhop13Application.java
@@ -0,0 +1,13 @@
+package edu.voloshin.pawnhop13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pawnhop13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pawnhop13Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/voloshin/pawnhop13/Pawnhop13ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.voloshin.pawnhop13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pawnhop13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
